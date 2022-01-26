7ea5a3e9259215af43f2d5e9895c8c3135b5b7b5
imjacksdad <imjacksdad@gmail.com>
2022-01-26 11:53:30 -0600

    Update archive_csv_files.bat

--- archive_csv_files.bat
+++ archive_csv_files.bat
@@ -6,5 +6,5 @@ move \\tedfil01\BatchDEV\BSM\FTP\*invticket*.csv \\tedfil01\InformaticaDEV\Archi
 
 move \\tedfil01\BatchDEV\BSM\FTP\*labfee*.csv \\tedfil01\InformaticaDEV\Archive\BSM
 
-move \\tedfil01\BatchDEV\BSM\FTP\*ocean*.csv \\tedfil01\InformaticaDEV\Archive\BSM
+rem move \\tedfil01\BatchDEV\BSM\FTP\*ocean*.csv \\tedfil01\InformaticaDEV\Archive\BSM
 
